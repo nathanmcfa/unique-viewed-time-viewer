b43102992dcdffd4e226702837fa12022789b773
Nathan McFarland <nathanmcfa@gmail.com>
2019-03-20 01:59:28 -0400

    no ticket: add more tests for unique viewed time creation

--- src/app/classes/model/unique-viewed-time.spec.ts
+++ src/app/classes/model/unique-viewed-time.spec.ts
@@ -11,7 +11,21 @@ describe('UniqueViewedTime', () => {
 
 			expect(uvt.totalMsec).toEqual(620);
 			expect(uvt.segments.length).toEqual(2);
+			expect(uvt.segments[0].startMsec).toEqual(0);
+			expect(uvt.segments[0].endMsec).toEqual(120);
+			expect(uvt.segments[1].startMsec).toEqual(500);
+			expect(uvt.segments[1].endMsec).toEqual(1000);
+		});
+
+		it('should create a UniqueViewedTime when there are many overlaps', () => {
+			const uvt: UniqueViewedTime = createUVT(videoData[2], [vfData[3], vfData[4], vfData[5], vfData[6], vfData[7]]);
+
+			expect(uvt.totalMsec).toEqual(2129);
 			expect(uvt.segments.length).toEqual(2);
+			expect(uvt.segments[0].startMsec).toEqual(0);
+			expect(uvt.segments[0].endMsec).toEqual(2000);
+			expect(uvt.segments[1].startMsec).toEqual(3700);
+			expect(uvt.segments[1].endMsec).toEqual(3829);
 		});
 
 	});
